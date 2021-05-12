6918243aef64d66f628bcf6d1135ae587ebe486f
Sultim Tsyrendashiev <sultim.tsyrendashiev@gmail.com>
2021-05-12 21:28:53 +0300

    - added class for level-specific info

---
+++ Sources/Engine/Raytracing/CustomInfo.cpp
@@ -0,0 +1,265 @@
+/* Copyright (c) 2021 Sultim Tsyrendashiev
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+
+#include "StdH.h"
+#include "CustomInfo.h"
+
+#include <Engine/Graphics/Texture.h>
+#include <Engine/Entities/Entity.h>
+#include <Engine/World/World.h>
+#include <Engine/Models/ModelObject.h>
+#include <Engine/Models/ModelData.h>
+#include <Engine/Light/LightSource.h>
+#include <Engine/Graphics/TextureEffects.h>
+
+
+#include <Engine/Base/ListIterator.inl>
+
+
+extern INDEX srt_bLightSphericalIgnoreEditorModels;
+
+
+constexpr float WORLD_BASE_FORCE_INVISIBLE_EPSILON = 0.5f;
+
+
+SSRT::CustomInfo::CustomInfo()
+{
+  waterTextureNames = 
+  {
+    "WaterBase",
+    "WaterFX",
+    "WaterFall01",
+    "WaterFall02",
+  };
+
+  fireTextureNames =
+  {
+    "Fire",
+  };
+
+  // Positions of unnecessary brushes (that also have "World Base" names)
+  worldBaseToIgnore =
+  {
+    { "01_Hatshepsut",        { 0, 1152, 64 }       },
+    { "02_SandCanyon",        { 224, 128, -312}     },
+    { "02_SandCanyon",        { 224, 128, -120}     },
+    { "02_SandCanyon",        { -98.5f, 113.5f, 18} },
+    { "04_ValleyOfTheKings",  { 0, 96, 0 }          },
+    { "05_MoonMountains",     { -64, 80, -64}       },
+    { "06_Oasis",             { 128, -16, 0}        },
+  };
+
+  // Positions of unnecessary brushes (that also have "World Base" names)
+  dirLightsToIgnore =
+  {
+    { "06_Oasis", "Temple Roof" },
+  };
+
+  const char *texToIgnore = "LightBeam.tex";
+}
+
+SSRT::CustomInfo::~CustomInfo()
+{}
+
+bool SSRT::CustomInfo::IsWaterTexture(CTextureData *ptd) const
+{
+  if (ptd == nullptr)
+  {
+    return false;
+  }
+
+  // if it's water effect texture, then it's true
+  if (ptd->td_ptegEffect != nullptr && ptd->td_ptegEffect->IsWater())
+  {
+    return true;
+  }
+
+  return IsTextureNameIn(ptd, waterTextureNames);
+}
+
+bool SSRT::CustomInfo::IsFireTexture(CTextureData *ptd) const
+{
+  if (ptd == nullptr)
+  {
+    return false;
+  }
+
+  // assume that fire texture is always an effect texture
+  if (ptd->td_ptegEffect == nullptr)
+  {
+    return false;
+  }
+
+  // must not be water
+  if (ptd->td_ptegEffect->IsWater())
+  {
+    return false;
+  }
+
+  return IsTextureNameIn(ptd, fireTextureNames);
+}
+
+bool SSRT::CustomInfo::HasModelFireTexture(CEntity *penModel) const
+{
+  if (penModel->en_RenderType != CEntity::RT_MODEL)
+  {
+    return false;
+  }
+
+  // when editor light sources are enabled, lights from fire textures 
+  // will be added, as they present in a world
+  if (!srt_bLightSphericalIgnoreEditorModels)
+  {
+    return false;
+  }
+
+  CModelObject *pmoModelObject = penModel->GetModelForRendering();
+
+  if (pmoModelObject == nullptr)
+  {
+    return false;
+  }
+
+  auto *td = (CTextureData *)pmoModelObject->mo_toTexture.GetData();
+  return IsFireTexture(td);
+}
+
+bool SSRT::CustomInfo::IsBrushIgnored(CEntity *penBrush) const
+{ 
+  // ugly way to remove terrain planes above the world
+  if (!(penBrush->en_ulFlags & ENF_ZONING) && penBrush->GetName() == "World Base")
+  {
+    // check if it's exactly those unnecessary polygons
+    for (const auto &ignore : worldBaseToIgnore)
+    {
+      // if not for the current world
+      if (penBrush->GetWorld() == nullptr || penBrush->GetWorld()->wo_fnmFileName.FileName() != ignore.worldName.c_str())
+      {
+        continue;
+      }
+
+      const FLOAT3D &p = penBrush->GetPlacement().pl_PositionVector;
+
+      bool isInside = (p - ignore.position).ManhattanNorm() < WORLD_BASE_FORCE_INVISIBLE_EPSILON;
+
+      if (isInside)
+      {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+bool SSRT::CustomInfo::IsDirectionalLightIgnored(const CLightSource *plsLight) const
+{
+  CEntity *pen = plsLight->ls_penEntity;
+
+  if (pen == nullptr || pen->GetWorld() == nullptr)
+  {
+    return false;
+  }
+
+  const auto &wldName = pen->GetWorld()->wo_fnmFileName.FileName();
+  const auto &ltName = plsLight->ls_penEntity->GetName();
+
+  for (const auto &ignore : dirLightsToIgnore)
+  {
+    if (wldName == ignore.worldName.c_str() && ltName == ignore.entityName.c_str())
+    {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool SSRT::CustomInfo::IsSphericalLightIgnored(const CLightSource *plsLight) const
+{
+  //UBYTE h, s, v;
+  //ColorToHSV(plsLight->GetLightColor(), h, s, v);
+
+  //// ignore dim lights
+  //if (h < srt_iLightSphericalHSVThresholdHLower ||
+  //    h > srt_iLightSphericalHSVThresholdHUpper ||
+  //    v < srt_iLightSphericalHSVThresholdVLower ||
+  //    v > srt_iLightSphericalHSVThresholdVUpper)
+  //{
+  //  return true;
+  //}
+
+  CEntity *pen = plsLight->ls_penEntity;
+
+  if (HasLightEntityVertices(pen))
+  {
+    return false;
+  }
+
+  if (HasLightEntityVertices(pen->GetParent()))
+  {
+    return false;
+  }
+
+  {
+    FOREACHINLIST(CEntity, en_lnInParent, pen->en_lhChildren, itenChild)
+    {
+      if (HasLightEntityVertices(itenChild))
+      {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+bool SSRT::CustomInfo::IsTextureNameIn(CTextureData *ptd, const std::vector<std::string> &collection)
+{
+  for (const auto &name : collection)
+  {
+    if (ptd->GetName().FileName() == name.c_str())
+    {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool SSRT::CustomInfo::HasLightEntityVertices(CEntity *pen) const
+{
+  if (pen == nullptr)
+  {
+    return false;
+  }
+
+  if (pen->GetRenderType() != CEntity::RT_MODEL)
+  {
+    if (pen->GetRenderType() != CEntity::RT_EDITORMODEL)
+    {
+      return false;
+    }
+
+    if (srt_bLightSphericalIgnoreEditorModels)
+    {
+      return false;
+    }
+  }
+
+  CModelData *md = pen->GetModelObject()->GetData();
+
+  return md != nullptr && md->md_VerticesCt > 0;
+}
---
+++ Sources/Engine/Raytracing/CustomInfo.h
@@ -0,0 +1,69 @@
+/* Copyright (c) 2021 Sultim Tsyrendashiev
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <Engine/Math/Vector.h>
+
+namespace SSRT
+{
+
+class CustomInfo
+{
+public:
+  CustomInfo();
+  ~CustomInfo();
+
+  CustomInfo(const CustomInfo &other) = delete;
+  CustomInfo(CustomInfo &&other) noexcept = delete;
+  CustomInfo &operator=(const CustomInfo &other) = delete;
+  CustomInfo &operator=(CustomInfo &&other) noexcept = delete;
+
+  bool IsWaterTexture(CTextureData *ptd) const;
+  bool IsFireTexture(CTextureData *ptd) const;
+  bool HasModelFireTexture(CEntity *penModel) const;
+  bool IsBrushIgnored(CEntity *penBrush) const;
+  bool IsDirectionalLightIgnored(const CLightSource *plsLight) const;
+  bool IsSphericalLightIgnored(const CLightSource *plsLight) const;
+
+private:
+  static bool IsTextureNameIn(CTextureData *ptd, const std::vector<std::string> &collection);
+  bool HasLightEntityVertices(CEntity *pen) const;
+
+private:
+  struct WorldBaseIgnore
+  {
+    std::string worldName;
+    FLOAT3D position;
+  };
+
+  struct LightIgnore
+  {
+    std::string worldName;
+    std::string entityName;
+  };
+
+private:
+  std::vector<std::string> waterTextureNames;
+  std::vector<std::string> fireTextureNames;
+  std::vector<WorldBaseIgnore> worldBaseToIgnore;
+  std::vector<LightIgnore> dirLightsToIgnore;
+
+};
+
+}
